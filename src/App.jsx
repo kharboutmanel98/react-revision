cf69023119c317f333d8561a27ace5421e23ec83
kharboutmanel98 <kharbout2017@gmail.com>
2023-12-07 21:16:09 +0100

    Revision  useState , useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
 import { UserInfo, Container, UserForm } from "./components";
-
-const userData = [
-  {
-    id: 1,
-    name: "Yasmine",
-    age: "06",
-    location: "France",
-    phone: "2154862",
-  },
-  {
-    id: 2,
-    name: "Yahia",
-    age: "08",
-    location: "England",
-    phone: "584862",
-  },
-  {
-    id: 3,
-    name: "Youssef",
-    age: "09",
-    location: "US",
-    phone: "2154862",
-  },
-];
+import { useState } from "react";
 
 const App = () => {
+  const [userData, setUserData] = useState([
+    {
+      id: 1,
+      name: "Yasmine",
+      age: "06",
+      location: "France",
+      phone: "2154862",
+    },
+    {
+      id: 2,
+      name: "Yahia",
+      age: "08",
+      location: "England",
+      phone: "584862",
+    },
+    {
+      id: 3,
+      name: "Youssef",
+      age: "09",
+      location: "US",
+      phone: "2154862",
+    },
+  ]);
   const userList = userData.map((user) => (
     <UserInfo
       key={user.id}
@@ -34,10 +34,14 @@ const App = () => {
       phone={user.location}
     />
   ));
+  const addUser = (newUser) => {
+    newUser.id = Math.floor(Math.random() * 100);
+    setUserData([...userData, newUser]);
+  };
   return (
     <div>
       <Container>
-        <UserForm />
+        <UserForm addUser={addUser} />
       </Container>
       <Container>{userList}</Container>
     </div>
--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,33 +1,50 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import styles from "./styles.module.css";
-
 const { classForm } = styles;
+import { v4 as uuidv4 } from "uuid";
 
-const UserForm = () => {
-  const [form, setForm] = useState({
-    name: "",
-    age: "",
-    location: "",
-    phone: "",
-  });
+const intState = {
+  id: uuidv4(),
+  name: "",
+  age: "",
+  location: "",
+  phone: "",
+};
 
-  const inputHandler = (event) => {
+const UserForm = ({ addUser }) => {
+  const [form, setForm] = useState(intState);
+  const focusInput = useRef("test");
+
+  const handleChangeInput = (event) => {
     const { name, value } = event.target;
 
-    setForm((prev) => {
-      return { ...prev, [name]: value };
-    });
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  useEffect(() => {
+    focusInput.current.focus();
+  }, []);
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    addUser(form);
+    setForm(intState);
+    // console.log(testInput.current.value)
   };
 
   return (
     <div className={classForm}>
-      <form>
+      <form onSubmit={formHandler}>
         <label htmlFor="name">Name</label>
         <input
           type="text"
           name="name"
           value={form.name}
-          onChange={inputHandler}
+          onChange={(e) => handleChangeInput(e)}
+          ref={focusInput}
         />
 
         <label htmlFor="age">Age</label>
@@ -35,7 +52,7 @@ const UserForm = () => {
           type="text"
           name="age"
           value={form.age}
-          onChange={inputHandler}
+          onChange={(e) => handleChangeInput(e)}
         />
 
         <label htmlFor="location">Location</label>
@@ -43,7 +60,7 @@ const UserForm = () => {
           type="text"
           name="location"
           value={form.location}
-          onChange={inputHandler}
+          onChange={(e) => handleChangeInput(e)}
         />
 
         <label htmlFor="phone">Phone</label>
@@ -51,7 +68,7 @@ const UserForm = () => {
           type="text"
           name="phone"
           value={form.phone}
-          onChange={inputHandler}
+          onChange={(e) => handleChangeInput(e)}
         />
 
         <input type="submit" value="submit" />
